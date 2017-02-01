50ca3fe931f4bfc07c635048328512bd4224936b
webdesigngse <webdesign@gse.nl>
2017-02-01 16:31:01 +0100

    00006
    mainscreen titel naar clock

---
+++ public/_includes/js/functions.js
@@ -0,0 +1,33 @@
+var socket      = io();
+
+
+
+function updateClock() {
+ 	var currentTime = new Date();
+  	var currentHours   = currentTime.getHours ( );
+  	var currentMinutes = currentTime.getMinutes ( );
+  	var currentSeconds = currentTime.getSeconds ( );
+
+  	/* Pad the minutes and seconds with leading zeros, if required */
+  	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  	/* Convert an hours component of "0" to "12" */
+  	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+  	/* Compose the string for display */
+  	var currentTimeString ="[&nbsp;" + currentHours + ":" + currentMinutes + ":" + currentSeconds + "&nbsp;]";
+
+   	$("#clock").html(currentTimeString);
+ }
+
+
+
+
+
+
+
+ $(document).ready(function() {
+    setInterval('updateClock()', 1000);
+    // setInterval('holiday()'    , 1000);
+ });
