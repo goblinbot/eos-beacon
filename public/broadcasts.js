32301255fa5bef89f920143803bb7b0ae5c554ae
webdesigngse <webdesign@gse.nl>
2017-06-15 14:30:53 +0200

    maati override

--- public/broadcasts.js
+++ public/broadcasts.js
@@ -20,32 +20,38 @@ function broadcastObj(title, file, priority, duration, colorscheme) {
   this.colorscheme = colorscheme;
 }
 
-// de broadcasts !
+/* Systeem */
 var bcdefault = new broadcastObj("Broadcast Initialise","bcdefault",1,"0","0");
 var bcreset = new broadcastObj("Clear","bcdefault",99,"0","0");
-var bcsystemscan = new broadcastObj("System Diagnostics","bcsystemscan",88,"60000","0");
 
+/* keuken crew */
 var bclunch = new broadcastObj("Lunch announcement","bclunch",2,"0","0");
 var bcdinner = new broadcastObj("Dinner announcement","bcdinner",2,"0","0");
-var bchydrate = new broadcastObj("Hydration Reminder","bchydrate",2,"0","0");
 var bcdishes = new broadcastObj("Dishes reminder","bcdishes",2,"0","0");
-var bcmeeting = new broadcastObj("Meeting in Main","bcmeeting",4,"0","0");
 
+/* andere PSA's */
+var bchydrate = new broadcastObj("Hydration Reminder","bchydrate",2,"0","0");
+var bcmeeting = new broadcastObj("Meeting in Main","bcmeeting",4,"0","0");
 var bcmedical = new broadcastObj("Medical Request","bcmedical",4,"0","0");
 
+/* hazards */
 var bcviral = new broadcastObj("Unknown Viral Pathogens detected","bcviral",8,"0","hazard");
 var bcbiohazard = new broadcastObj("Envirnomental Hazard detected","bcbiohazard",8,"0","hazard");
 var bcpsyhazard = new broadcastObj("Psy-hazard detected","bcpsyhazard",8,"0","psyhazard");
 var bchostile = new broadcastObj("Enemy Contact","bchostile",8,"0","attack");
 
-/* portal rights */
+/* portal */
 var bcportalinc = new broadcastObj("Portal Incoming","bcportalinc",3,"30000","0");
 var bcportalout = new broadcastObj("Portal Outgoing","bcportalout",3,"17500","0");
 
-/* Overlord only */
+/* Overlord-only */
 var bclowpower = new broadcastObj("POWER SUPPLY WARNING","bclowpower",8,"0","gray");
 var bcreactorcrit = new broadcastObj("Reactor Critical","bcreactorcrit",9,"0","attack");
 var bchackattack = new broadcastObj("Security Breach","bchackattack",9,"0","0");
 
-/* Maati's development */
-var bcmaati = new broadcastObj("BC_MAATI","bcmaati",9,"0","0");
+/* Maati's Eos IT toolbox */
+var bcsystemscan = new broadcastObj("System Diagnostics","bcsystemscan",88,"60000","0");
+var bcfreezeall = new broadcastObj("ADMIN.OVERRIDE","bcfreezeall",100,"0","0");
+
+/* Testomgeving */
+var bcmaati = new broadcastObj("test","bcmaati",9,"0","0");
